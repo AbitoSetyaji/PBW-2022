f0c35e83164cc4d4e61f1912bcc9899a4d312f00
AbitoSetyaji <113467301+AbitoSetyaji@users.noreply.github.com>
2023-01-02 19:17:34 +0700

    Delete Latihan JavaScript directory

--- Latihan JavaScript/eksternal-js/kode.js
+++
@@ -1,5 +0,0 @@
-alert("Hello, ini adalah program JS eksternal!");
-var title = "Belajar Pemrograman Javascript"; 
-var siteName = "Belajar Kode"; 
-var url = "http://dinus.ac.id"; 
-var visitorCount = 1000; 
