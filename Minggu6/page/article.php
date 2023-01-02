fcfb2445f81e251abfb836e6de5e55be837899bd
AbitoSetyaji <113467301+AbitoSetyaji@users.noreply.github.com>
2023-01-02 19:11:14 +0700

    Add files via upload

---
+++ Minggu6/page/article.php
@@ -0,0 +1,10 @@
+<article class="article">
+        <h2 class="h2p" align ="center">Sinopsis</h2>
+        <p>
+            Status kesepian itu menyebalkan bagi Jono, dia benar-benar menginginkan pakara selama lima tahun terakhir hidupnya, kebosanan kesepiannya akhirnya membuatnya memutuskan untuk mencari anak laki-laki sekarang. Berbagai metode digunakan sampai, akhirnya, sahabatnya membantunya, Niko mencari keberuntungan dalam mendapatkan pakaras di dunia maya.
+
+Dengan satu upaya, Yono segera bertemu dengan teman-teman, kenalan tidak berakhir di sana, dan mereka tetap berhubungan sampai tahap berikutnya. Namun, semuanya tidak berfungsi dengan sempurna, kekecewaan dan rasa pahit tidak bisa lagi dihindari. Hal yang sama berlaku untuk Nico, yang harus memiliki hati yang hancur. Tetapi apakah pertempuran berhenti di sini? apa status dari single yang berpakaian lagi ???
+        </p>
+        <h2 class="h2p" align="center"> hallo</h2>
+        <p>Status kesepian itu menyebalkan bagi Jono, dia benar-benar menginginkan pakara selama lima tahun terakhir hidupnya, kebosanan kesepiannya akhirnya membuatnya memutuskan untuk mencari anak laki-laki sekarang. Berbagai metode digunakan sampai, akhirnya, sahabatnya membantunya, Niko mencari keberuntungan dalam mendapatkan pakaras di dunia maya. Dengan satu upaya, Yono segera bertemu dengan teman-teman, kenalan tidak berakhir di sana, dan mereka tetap berhubungan sampai tahap berikutnya. Namun, semuanya tidak berfungsi dengan sempurna, kekecewaan dan rasa pahit tidak bisa lagi dihindari. Hal yang sama berlaku untuk Nico, yang harus memiliki hati yang hancur. Tetapi apakah pertempuran berhenti di sini? apa status dari single yang berpakaian lagi ???</p>
+    </article>
